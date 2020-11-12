77a5cd999e003387f5b48f12ea6dad3a73e76404
MikBom <72486901+MikBom@users.noreply.github.com>
2020-11-12 17:50:42 +0200

    Add files via upload

---
+++ message.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+
+<link rel="stylesheet" href="tyyli.css"> 
+
+<body>
+
+<h1>Thank you!</h1>
+<a href="index.html" class="padded">Front page</a>
+<?php
+echo "Thanks for the message, we will be read it with pleasure :)";
+?> 
+
+</body>
+</html>
