157b0123b808c5c01def7098c5603271b840b8d0
MikBom <72486901+MikBom@users.noreply.github.com>
2020-12-06 20:45:58 +0200

    Update mail.php

--- mail.php
+++ mail.php
@@ -1,7 +1,46 @@
-<html>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="utf-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+<title>12 - Final page</title>
+</head>
 <body>
 
+<div style="margin-left: 20px;">
+	<h1>Wiikintie 2 A 2 family</h1>
+		<p>This page shows what is happening<br>in Wiikintie 2 A 2</p>
+	</div>
+	<hr>
+<nav class="navbar navbar-inverse">
+  <div class="container-fluid">
+    
+    <ul class="nav navbar-nav">
+      <li class="active"><a href="index.html">About us</a></li>
+      <li><a href="photos.html">Photos from appartment</a></li>
+      <li><a href="hobbies.html">Hobbies</a></li>
+      <li><a href="contact.html">Contact us</a></li>
+    </ul>
+  </div>
+</nav>
+<div style="margin-left: 20px">
+
 Your message was <?php echo $_POST["w3review"]; ?><br>
+
+<footer class="page-footer font-small">
+  <div style="background-color: #333; color: white; text-align: center;">
+ <div>© R0244-3007 Mikko Boman 2005542
+    </div></div>
+  
+</footer>
+
+
+
+
+
 
 </body>
 </html> 
