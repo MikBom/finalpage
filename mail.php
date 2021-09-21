d16ef38606d054452705adef0a601cd92f211149
MikBom <72486901+MikBom@users.noreply.github.com>
2021-09-21 18:10:24 +0300

    Update mail.php

--- mail.php
+++ mail.php
@@ -1,60 +1,6 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-<meta charset="utf-8" />
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
-<title>About us</title>
-</head>
-<body>
-
-<div style="margin-left: 20px;">
-	<h1>Wiikintie 2 A 2 family</h1>
-		<p>This page shows what is happening<br>in Wiikintie 2 A 2</p>
-	</div>
-	<hr>
-<nav class="navbar navbar-inverse" align="center">
-  <div class="container-fluid">
-    
-    <ul class="nav navbar-nav">
-      <li class="active"><a href="index.html">About us</a></li>
-      <li><a href="photos.html">Photos from appartment</a></li>
-      <li><a href="hobbies.html">Hobbies</a></li>
-      <li><a href="contact.html">Contact us</a></li>
-    </ul>
-  </div>
-</nav>
-<div style="margin-left: 20px">
- <div class="text-center">
-	<h2>About us</h2>
-	
-	<img src="images/both.jpg"  alt="both" class="img-circle" width="300" height="300">
-	<p class="h4">We are two persons who have been living together ten years. <br>
-	We have good harmony together and we love to be living in this appartment.<br>
-	Good location in centre of Kirkkonummi with renovated appartment is<br>
-	great for us. The shopping mall, services and hobbies are near. </p>
-	
-	<p class="h4">In these pages you can find what kind of our appartment was before<br>
-	and after we did small surface fixings for it. Also you have possibility<br>
-	 to check our hobbies and contact details for more information.</p>
-	 
-	 <p class="h4"> This appartment was third one we were checking when we were looking <br>
-	 the house for us. Our parents was saying that we should not buy the <br>
-	 first one we go to check, so we granted their wishes and bought the third one :)<br>
-	 This decision has been one of the best we have ever done</p>
-	</div>
-</div>
-
-<footer class="page-footer font-small">
-  <div style="background-color: #333; color: white; text-align: center;">
- <div>© R0244-3007 Mikko Boman 2005542
-    </div></div>
-  
-</footer>
-
-
-
-</body>
-</html>
+<?php 
+		
+		$teksti = $_POST['w3review'];
+		
+		echo ("Moi, viestisi oli: ". "$teksti ". "Kiitos viestistäsi!);
+?>
